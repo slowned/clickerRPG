88165a77c9f9e1d54ede07c73da2ab1af98cb80c
mariano <mllambay@hindsightsolutions.net>
2023-04-09 21:38:51 -0300

    a lot of changes

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+  public static bool gameIsPaused = false;
+  public GameObject pauseMenuUI;
+  
+  void Start() {
+      
+  }
+
+  void Update() {
+    if (Input.GetKeyDown(KeyCode.Escape)) {
+      if (gameIsPaused) {
+        Resume();
+      } else {
+        Pause();
+      }
+    }
+      
+  }
+
+  public void Resume() {
+    pauseMenuUI.SetActive(false);
+    Time.timeScale = 1f;
+    gameIsPaused = false;
+  }
+
+  void Pause() { 
+    pauseMenuUI.SetActive(true);
+    Time.timeScale = 0f;
+    gameIsPaused = true;
+  }
+
+  public void LoadMenu() {
+    Debug.Log("Loading menu...");
+  }
+  public void QuitGame() {
+    Debug.Log("Quit Game...");
+  }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,61 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    private EnemyStats enemyStatsScript;
-    public GameObject enemy;
-    public bool fighting = false;
-    private int baseDamage = 5;
-    private int damage = 5;
+  public Animator animator;
 
-    public int health, maxHealth;
-    public int baseHealth = 200;
+  private EnemyStats enemyStatsScript;
+  public GameObject enemy;
+  public bool fighting = false;
+  private int baseDamage = 5;
+  private int damage = 5;
 
-    public HealthBarController healthBar;
+  public int health, maxHealth;
+  public int baseHealth = 200;
 
-    // AUTO-ATTACK
-    private float autoAttackCooldown = 3.0f;
-    public float autoAttackCurTime;
-    public bool canAutoAttack;
+  public HealthBarController healthBar;
 
-    // LeveUpController
-    public int level = 1;
-    public int experience = 0;
-    public int experienceToNextLevel;
+  // AUTO-ATTACK
+  private float autoAttackCooldown = 3.0f;
+  public float autoAttackCurTime;
+  public bool canAutoAttack;
 
-    void Start() {
-      experienceToNextLevel = GetExperienceToNextlevel();
-      health = baseHealth * level;
-      maxHealth = health; 
-      healthBar.FullHp(maxHealth);
-    }
+  // LeveUpController
+  public int level = 1;
+  public int experience = 0;
+  public int experienceToNextLevel;
 
-    void Update() {
-      // AUTO ATTACK
-      autoAttackCurTime += Time.deltaTime;
+  void Start() {
+    experienceToNextLevel = GetExperienceToNextlevel();
+    health = baseHealth * level;
+    maxHealth = health; 
+    healthBar.FullHp(maxHealth);
 
-      if (enemy != null && CanAutoAttack()) {
-        float enemyHp = enemyStatsScript.GetDamage(damage);
-        if (enemyHp < 1) {
-          enemy = null;
-          fighting = false;
-        }
+  }
+
+  void Update() {
+    // AUTO ATTACK
+    autoAttackCurTime += Time.deltaTime;
+
+    if (enemy != null && CanAutoAttack()) {
+      float enemyHp = enemyStatsScript.GetDamage(damage);
+      if (enemyHp < 1) {
+        enemy = null;
+        fighting = false;
+        animator.SetBool("IsFighting", false);
       }
     }
-
-    public void LevelUp() {
-      Debug.Log("SUBI D NIVEL");
-      level += 1;
-      experience = 0;
-      experienceToNextLevel = GetExperienceToNextlevel();
-      // ver bien la formula
-      damage = baseDamage + level;
-
-      maxHealth += 10 * level;
-      health += maxHealth * 5 / 100;
-
-      healthBar.SetMaxHealth(maxHealth);
-      healthBar.RestoreHp(maxHealth * 5 / 100);
   }
+
+  public void LevelUp() {
+    level += 1;
+    experience = 0;
+    experienceToNextLevel = GetExperienceToNextlevel();
+    // ver bien la formula
+    damage = baseDamage + level;
+
+    maxHealth += 10 * level;
+    health += maxHealth * 5 / 100;
+
+    healthBar.SetMaxHealth(maxHealth);
+    healthBar.RestoreHp(maxHealth * 5 / 100);
+}
 
   public void GiveExperience(int _exp) {
     experience += _exp;
@@ -73,50 +76,38 @@ public class PlayerController : MonoBehaviour {
 
   private void OnCollisionEnter2D(Collision2D other) {
     if (other.gameObject.CompareTag("Enemy")) {
+      animator.SetBool("IsFighting", true);
       fighting = true;
       enemy = other.gameObject;
       enemyStatsScript = enemy.GetComponent<EnemyStats>();
       enemyStatsScript.GenerateAggro(gameObject);
-      //enemyStatsScript = GameObject.FindWithTag("Enemy").GetComponent<EnemyStats>();
-
-      /* if (healthBar) { */
-        /*     healthBar.onTakeDamage(20); */
-        /* } */
-      }
+    }
   }
 
-  private void OnCollisionEnter(Collision other) {
-    // empieza animacion de pegar
-    if (other.gameObject.CompareTag("Enemy")) {
-      fighting = true;
-      enemy = other.gameObject;
-      enemyStatsScript = enemy.GetComponent<EnemyStats>();
-      enemyStatsScript.GenerateAggro(gameObject);
-      //enemyStatsScript = GameObject.FindWithTag("Enemy").GetComponent<EnemyStats>();
+  public void TakeDamage(int damage) {
+    health -= damage;
+    healthBar.OnTakeDamage(damage);
+    animator.SetTrigger("IsBeaten");
+    if (health < 1) {
+      animator.SetBool("IsDead", true);
+      // Destroy(gameObject);
 
-      /* if (healthBar) { */
-        /*     healthBar.onTakeDamage(20); */
-        /* } */
-      }
-    } 
-
-    public void TakeDamage(int damage) {
-      health -= damage;
-      healthBar.OnTakeDamage(damage);
-      if (health < 1) {
-        Destroy(gameObject);
-        GameManager gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
-        gameManager.GameOver();
-      }
+      GameManager gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+      gameManager.GameOver();
     }
+  }
 
-    public bool CanAutoAttack() {
-      if(autoAttackCurTime >= autoAttackCooldown) {
-        autoAttackCurTime = 0;
-        return true;
-      } else {
-        return false;
+  public bool CanAutoAttack() {
+    if(autoAttackCurTime >= autoAttackCooldown) {
+      autoAttackCurTime = 0;
+      if ( enemy != null ) { 
+        animator.SetTrigger("Attack1"); 
       }
+      canAutoAttack = true;
+      return true;
+    } else {
+      canAutoAttack = false;
+      return false;
     }
-
+  }
 }
